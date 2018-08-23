c45ca3d84510cc1a5f6ce765b6a2f8d8409be0e0
rohit sahay <rohitsahay.199626@gmail.com>
2018-08-23 12:50:21 +0530

    new commit

--- app/src/main/java/com/example/rohitsahay/jauntbee20/MainActivity.java
+++ app/src/main/java/com/example/rohitsahay/jauntbee20/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Interpolator;
 import android.widget.Button;
 import android.widget.Toast;
 import android.widget.Toolbar;
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private android.support.v7.widget.Toolbar mToolbar;
-    //private Button mButton;
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
         mToolbar = findViewById(R.id.nav_action);
         setSupportActionBar(mToolbar);
 
+        mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapIntent = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(mapIntent);
+            }
+        });
 
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
--- app/src/main/java/com/example/rohitsahay/jauntbee20/MapsActivity.java
+++ app/src/main/java/com/example/rohitsahay/jauntbee20/MapsActivity.java
@@ -1,60 +1,64 @@
 package com.example.rohitsahay.jauntbee20;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.provider.Settings;
+import android.os.Build;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.database.DatabaseReference;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks,
-GoogleApiClient.OnConnectionFailedListener,
-        LocationListener{
+import Model.MyPlaces;
+import Model.Results;
+import Remote.IGoogleAPIService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
+{
+
+    private static final int MY_PERMISSION_CODE = 1000 ;
     private GoogleMap mMap;
 
-    //Play Services
-
-    private static final int MY_PRMISSION_REQUEST_CODE = 7000;
-    private static final int PLAY_SERVICE_RES_REQUEST = 7001;
-
-    private LocationRequest mLocationRequest;
-    private GoogleApiClient mGoogleAPIClient;
+    private double latitude,longitude;
     private Location mLastLocation;
+    private Marker mMarker;
 
 
-    private static int UPDATE_INTERVAL = 5000;
-    public static int FASTEST_INTERVAL = 3000;
-    public static int DISPLACEMENT = 10 ;
-
-    DatabaseReference drivers;
-
+    //New Location
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationCallback locationCallback;
+    private LocationRequest mLocationRequest;
 
 
-
-
-
+    IGoogleAPIService mService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,37 +69,263 @@ GoogleApiClient.OnConnectionFailedListener,
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //Init Service
+        mService = Common.getGoogleAPIService();
+
+        //Request Runtime Permission
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            checkLocationPermission();
+        }
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nevigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.action_hospital:
+                        nearByPlace("hospital");
+                        Toast.makeText(MapsActivity.this, "hospital", Toast.LENGTH_SHORT).show();
+
+                        break;
+
+                    case R.id.action_market:
+                        nearByPlace("market");
+                        Toast.makeText(MapsActivity.this, "market", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_resturant:
+                        nearByPlace("police station");
+                        Toast.makeText(MapsActivity.this, "police station", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_school:
+                        nearByPlace("school");
+                        Toast.makeText(MapsActivity.this, "school", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    default: break;
+                }
 
 
+
+                return true;
+            }
+        });
+
+        //Init Location
+        buildLocationCallback();
+        buildLocationRequest();
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationProviderClient.requestLocationUpdates(mLocationRequest,locationCallback, Looper.myLooper());
 
     }
 
+    @Override
+    protected void onStop() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        super.onStop();
+
+    }
+
+    private void buildLocationCallback() {
+        locationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                mLastLocation = locationResult.getLastLocation();
+
+                if(mMarker!=null)
+                    mMarker.remove();
+
+                latitude = mLastLocation.getLatitude();
+                longitude = mLastLocation.getLongitude();
+
+                LatLng latLng = new LatLng(latitude,longitude);
+                MarkerOptions markerOptions = new MarkerOptions()
+                        .position(latLng)
+                        .title("Your position")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                mMarker = mMap.addMarker(markerOptions);
+
+                //Move camera
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+
+
+
+
+
+
+            }
+        };
+
+    }
+
+
+    private void buildLocationRequest(){
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setSmallestDisplacement(10f);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+    }
+    private void nearByPlace(final String placeType) {
+
+        mMap.clear();
+        String url = getUrl(latitude,longitude,placeType);
+
+        mService.getNearByPlaces(url)
+                .enqueue(new Callback<MyPlaces>() {
+                    @Override
+                    public void onResponse(Call<MyPlaces> call, Response<MyPlaces> response) {
+                        if(response.isSuccessful()){
+
+                            for(int i=0 ; i<response.body().getResults().length ; i++){
+
+                                MarkerOptions markerOptions = new MarkerOptions();
+                                Results googlePlace = response.body().getResults()[i];
+                                double lat = Double.parseDouble(googlePlace.getGeometry().getLocation().getLat());
+                                double lng = Double.parseDouble(googlePlace.getGeometry().getLocation().getLng());
+                                String placeName = googlePlace.getName();
+                                String vicinity = googlePlace.getVicinity();
+                                LatLng latLng = new LatLng(lat,lng);
+                                markerOptions.position(latLng);
+                                markerOptions.title(placeName);
+                                if(placeType.equals("hospital"))
+                                    markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_local_hospital_black_24dp));
+
+                                else if(placeType.equals("market"))
+                                    markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_shoppning_cart_black_24dp));
+
+                                else if(placeType.equals("police station"))
+                                    markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_restaurant_black_24dp));
+
+                                else if(placeType.equals("market"))
+                                    markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_school_black_24dp));
+                                else  markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+
+
+
+
+                                //add marker
+                                mMap.addMarker(markerOptions);
+                                // move camera
+
+                                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                                mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
+
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<MyPlaces> call, Throwable t) {
+
+                    }
+                });
+    }
+
+    private String getUrl(double latitude, double longitude, String placeType) {
+
+        StringBuilder googlePlacesUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlacesUrl.append("location="+latitude+","+longitude);
+        googlePlacesUrl.append("&radius="+10000);
+        googlePlacesUrl.append("&type="+placeType);
+        googlePlacesUrl.append("&sensor=true");
+        googlePlacesUrl.append("&key="+getResources().getString(R.string.browser_key));
+        Log.d("getUrl",googlePlacesUrl.toString());
+        return googlePlacesUrl.toString();
+    }
+
+    private boolean checkLocationPermission() {
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION))
+                ActivityCompat.requestPermissions(this,new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                },MY_PERMISSION_CODE);
+            else
+                ActivityCompat.requestPermissions(this,new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                },MY_PERMISSION_CODE);
+            return false;
+        }
+
+
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+
+            case MY_PERMISSION_CODE:
+            {
+
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+
+                    if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED)
+                    {
+
+                        mMap.setMyLocationEnabled(true);
+                        buildLocationCallback();
+                        buildLocationRequest();
+
+                        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+                        fusedLocationProviderClient.requestLocationUpdates(mLocationRequest,locationCallback, Looper.myLooper());
+                    }
+                }
+
+
+            }
+            break;
+        }
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        //Init Google Play Services
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            {
+
+                mMap.setMyLocationEnabled(true);
+            }
+        }
+        else{
+
+            mMap.setMyLocationEnabled(true);
+
+        }
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                Toast.makeText(MapsActivity.this, "I don't know", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
 
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
 
 
-    }
 
-    @Override
-    public void onConnectionSuspended(int i) {
 
-    }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
-    }
 
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
 }
